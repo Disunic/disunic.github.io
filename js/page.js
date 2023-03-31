ab9ca841ada2e0a0523557eec210026036612021
Disunic <83856385+Disunic20@users.noreply.github.com>
2023-03-31 16:23:22 +0530

    Add files via upload

--- js/page.js
+++ js/page.js
@@ -1,5 +1,6 @@
 let searchch = document.getElementById("searchch");
-let engine = ""
+let searchch2 = document.getElementById("searchch2");
+let engine = "";
 function changesearchengine() {
     document.getElementById("searchengines").style.display = "block"
     document.getElementById("main").style.opacity = "10%"
@@ -10,14 +11,38 @@ function changesearchenginecls() {
     document.getElementById("main").style.opacity = "100"
     document.getElementById("main2").style.opacity = "100"
 }
+function changesearchengine2() {
+    document.getElementById("searchengines").style.display = "block"
+    document.getElementById("main").style.opacity = "10%"
+    document.getElementById("main2").style.opacity = "10%"
+}
+function changesearchenginecls2() {
+    document.getElementById("searchengines").style.display = "none"
+    document.getElementById("logo").style.display = "none"
+    document.getElementById("main").style.opacity = "100"
+    document.getElementById("main2").style.opacity = "100"
+}
 function googlesearchforlogic() {
     engine = "Google"
     localStorage.removeItem("engine");
     localStorage.setItem("engine", "Google");
     let searchch = document.getElementById("searchch");
+    let searchch2 = document.getElementById("searchch2");
     searchch.src = "./img/external/googlelogo.png";
+    searchch2.src = "./img/external/googlelogo.png";
     changesearchenginecls()
     noneInformationdesign();
+    check2();
+}
+function googlesearchforlogic2() {
+    engine = "Google"
+    localStorage.removeItem("engine");
+    localStorage.setItem("engine", "Google");
+    let searchch = document.getElementById("searchch");
+    let searchch2 = document.getElementById("searchch2");
+    searchch.src = "./img/external/googlelogo.png";
+    searchch2.src = "./img/external/googlelogo.png";
+    changesearchenginecls2();
 }
 function informationsearchforlogic() {
     engine = "Information"
@@ -34,66 +59,136 @@ function bingsearchforlogic() {
     localStorage.removeItem("engine");
     localStorage.setItem("engine", "Bing");
     let searchch = document.getElementById("searchch");
+    let searchch2 = document.getElementById("searchch2");
     document.getElementById("searchengines").style.display = "none"
     searchch.src = "./img/external/binglogo.jpeg";
+    searchch2.src = "./img/external/binglogo.jpeg";
     changesearchenginecls()
     noneInformationdesign();
+    check2();
+}
+function bingsearchforlogic2() {
+    engine = "Bing"
+    localStorage.removeItem("engine");
+    localStorage.setItem("engine", "Bing");
+    let searchch = document.getElementById("searchch");
+    let searchch2 = document.getElementById("searchch2");
+    document.getElementById("searchengines").style.display = "none"
+    searchch.src = "./img/external/binglogo.jpeg";
+    searchch2.src = "./img/external/binglogo.jpeg";
+    changesearchenginecls2()
 }
 function bravesearchforlogic() {
     engine = "Brave"
     localStorage.removeItem("engine");
     localStorage.setItem("engine", "Brave");
     let searchch = document.getElementById("searchch");
+    let searchch2 = document.getElementById("searchch2");
     document.getElementById("searchengines").style.display = "none"
     searchch.src = "./img/external/brave.png";
+    searchch2.src = "./img/external/brave.png";
     changesearchenginecls()
     noneInformationdesign();
+    check2();
+}
+function bravesearchforlogic2() {
+    engine = "Brave"
+    localStorage.removeItem("engine");
+    localStorage.setItem("engine", "Brave");
+    let searchch = document.getElementById("searchch");
+    let searchch2 = document.getElementById("searchch2");
+    document.getElementById("searchengines").style.display = "none"
+    searchch.src = "./img/external/brave.png";
+    searchch2.src = "./img/external/brave.png";
+    changesearchenginecls2()
 }
 function yahoosearchforlogic() {
     engine = "Yahoo"
     localStorage.removeItem("engine");
     localStorage.setItem("engine", "Yahoo");
     let searchch = document.getElementById("searchch");
+    let searchch2 = document.getElementById("searchch2");
     document.getElementById("searchengines").style.display = "none"
     searchch.src = "./img/external/yahoologo.jpeg";
+    searchch2.src = "./img/external/yahoologo.jpeg";
     changesearchenginecls()
     noneInformationdesign();
+    check2();
+}
+function yahoosearchforlogic2() {
+    engine = "Yahoo"
+    localStorage.removeItem("engine");
+    localStorage.setItem("engine", "Yahoo");
+    let searchch = document.getElementById("searchch");
+    let searchch2 = document.getElementById("searchch2");
+    document.getElementById("searchengines").style.display = "none"
+    searchch.src = "./img/external/yahoologo.jpeg";
+    searchch2.src = "./img/external/yahoologo.jpeg";
+    changesearchenginecls2()
 }
 function duckduckgosearchforlogic() {
     engine = "DuckDuckGo"
     localStorage.removeItem("engine");
     localStorage.setItem("engine", "DuckDuckGo");
     let searchch = document.getElementById("searchch");
+    let searchch2 = document.getElementById("searchch2");
     document.getElementById("searchengines").style.display = "none"
     searchch.src = "./img/external/duckduckgologo.png";
+    searchch2.src = "./img/external/duckduckgologo.png";
     changesearchenginecls()
     noneInformationdesign();
+    check2();
+}
+function duckduckgosearchforlogic2() {
+    engine = "DuckDuckGo"
+    localStorage.removeItem("engine");
+    localStorage.setItem("engine", "DuckDuckGo");
+    let searchch = document.getElementById("searchch");
+    let searchch2 = document.getElementById("searchch2");
+    document.getElementById("searchengines").style.display = "none"
+    searchch.src = "./img/external/duckduckgologo.png";
+    searchch2.src = "./img/external/duckduckgologo.png";
+    changesearchenginecls2()
 }
 function metagersearchforlogic() {
     engine = "Metager"
     localStorage.removeItem("engine");
     localStorage.setItem("engine", "Metager");
     let searchch = document.getElementById("searchch");
+    let searchch2 = document.getElementById("searchch2");
     document.getElementById("searchengines").style.display = "none"
     searchch.src = "./img/external/metager.png";
+    searchch2.src = "./img/external/metager.png";
     changesearchenginecls()
     noneInformationdesign();
+    check2();
+}
+function metagersearchforlogic2() {
+    engine = "Metager"
+    localStorage.removeItem("engine");
+    localStorage.setItem("engine", "Metager");
+    let searchch = document.getElementById("searchch");
+    let searchch2 = document.getElementById("searchch2");
+    document.getElementById("searchengines").style.display = "none"
+    searchch.src = "./img/external/metager.png";
+    searchch2.src = "./img/external/metager.png";
+    changesearchenginecls2()
 }
 
-function Informationdesign(){
-    document.getElementById("logo").style.display ="none";
-    document.getElementById("searchcontent").style.display ="none";
-    document.getElementById("chatbox").style.display ="block";
-    document.getElementById("bidark").setAttribute("onclick","")
-    document.getElementById("main2").style.display ="none";
+function Informationdesign() {
+    document.getElementById("logo").style.display = "none";
+    document.getElementById("searchcontent").style.display = "none";
+    document.getElementById("chatbox").style.display = "block";
+    document.getElementById("bidark").setAttribute("onclick", "")
+    document.getElementById("main2").style.display = "none";
     onlight()
 }
-function noneInformationdesign(){
-    document.getElementById("logo").style.display ="inline-flex";
-    document.getElementById("searchcontent").style.display ="block";
-    document.getElementById("chatbox").style.display ="none";
-    document.getElementById("bidark").setAttribute("onclick","darkmode()")
-    document.getElementById("main2").style.display ="block";
+function noneInformationdesign() {
+    document.getElementById("logo").style.display = "inline-flex";
+    document.getElementById("searchcontent").style.display = "block";
+    document.getElementById("chatbox").style.display = "none";
+    document.getElementById("bidark").setAttribute("onclick", "darkmode()")
+    document.getElementById("main2").style.display = "block";
 }
 function check() {
     // for search
@@ -106,6 +201,7 @@ function check() {
     }
     else if (localStorage.getItem("engine") === "Information") {
         informationsearchforlogic()
+        Informationdesign()
     }
     else if (localStorage.getItem("engine") === "Brave") {
         bravesearchforlogic()
@@ -122,39 +218,59 @@ function check() {
     else {
         googlesearchforlogic()
     }
+}
+function check2() {
     // for mode
-    if (localStorage.getItem("mode") === "light"){
+    if (localStorage.getItem("mode") === "light") {
         document.getElementById("bilight").style.display = "none"
         console.log("Light Mode")
         onlight()
+        if (localStorage.getItem("engine") === "Information") {
+            Informationdesign()
+        }
+
     }
-    else if (localStorage.getItem("mode") === "dark"){
+    else if (localStorage.getItem("mode") === "dark") {
         document.getElementById("bidark").style.display = "none"
         console.log("Dark Mode")
         ondark()
+        if (localStorage.getItem("engine") === "Information") {
+            Informationdesign()
+        }
+
     }
     else if (window.matchMedia && window.matchMedia('(prefers-color-scheme: light)').matches) {
         document.getElementById("bilight").style.display = "none"
         console.log("Light Mode")
         onlight()
+        if (localStorage.getItem("engine") === "Information") {
+            Informationdesign()
+        }
+
     }
     else if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
         document.getElementById("bidark").style.display = "none"
         console.log("Dark Mode")
         ondark()
+        if (localStorage.getItem("engine") === "Information") {
+            Informationdesign()
+        }
+
     }
 }
-function lightmode(){
+
+
+function lightmode() {
     localStorage.removeItem("mode")
-    localStorage.setItem("mode","light")
+    localStorage.setItem("mode", "light")
     onlight()
 }
-function darkmode(){
+function darkmode() {
     localStorage.removeItem("mode")
-    localStorage.setItem("mode","dark")
+    localStorage.setItem("mode", "dark")
     ondark()
 }
-function onlight(){
+function onlight() {
     document.getElementById("bilight").style.display = "none"
     document.getElementById("bidark").style.display = "block"
     document.getElementById("body").style.background = "white"
@@ -181,7 +297,7 @@ function onlight(){
     // document.getElementById("").style.color = ""
     $('input').removeClass('inp');
 }
-function ondark(){
+function ondark() {
     document.getElementById("bilight").style.display = "block"
     document.getElementById("bidark").style.display = "none"
     document.getElementById("body").style.background = "#1f2533"
@@ -207,17 +323,50 @@ function ondark(){
     // document.getElementById("").style.color = ""
     $('input').addClass('inp');
 }
-function inpclick(){
+function inpclick() {
     document.getElementById("main2").style.display = "none";
     document.getElementById("logo").style.display = "none";
-    document.getElementById("searchcontent").style.marginTop = "85px";
-    document.getElementById("searchcontent").style.width = "90%";
+    document.getElementById("searchcontent").style.marginTop = "0px";
+    document.getElementById("searchcontent").style.width = "100%";
     document.getElementById("btn2").style.display = "block";
+    document.getElementById("searchcontent").style.animation = "ani1 500ms";
+    document.getElementById("searchch2").style.display = "block";
+    document.getElementById("nav").style.display = "none";
+    document.getElementById("searchcontent").style.borderRadius = "0px";
+    document.getElementById("searchbar").style.borderRadius = "0px";
+    document.getElementById("searchbar").style.height = "65px";
+    document.getElementById("info").style.display = "none"
+    document.getElementById("google").setAttribute("onclick", "googlesearchforlogic2(),googlesearch()")
+    document.getElementById("bing").setAttribute("onclick", "bingsearchforlogic2(),bingsearch()")
+    document.getElementById("brave").setAttribute("onclick", "bravesearchforlogic2(),bravesearch()")
+    document.getElementById("yahoo").setAttribute("onclick", "yahoosearchforlogic2(),yahoosearch()")
+    document.getElementById("duckduckgo").setAttribute("onclick", "duckduckgosearchforlogic2(),duckduckgosearch()")
+    document.getElementById("metager").setAttribute("onclick", "metagersearchforlogic2(),metagersearch()")
 }
-function inpclickback(){
+function inpclickback() {
     document.getElementById("main2").style.display = "";
     document.getElementById("logo").style.display = "inline-flex";
     document.getElementById("searchcontent").style.marginTop = "0px";
     document.getElementById("searchcontent").style.width = "85%";
+    document.getElementById("searchcontent").style.animation = "ani2 500ms";
     document.getElementById("btn2").style.display = "none";
+    document.getElementById("searchch2").style.display = "none";
+    document.getElementById("nav").style.display = "flex";
+    document.getElementById("searchcontent").style.borderTopRightRadius = "10px";
+    document.getElementById("searchcontent").style.borderTopLeftRadius = "10px";
+    document.getElementById("searchbar").style.borderTopLeftRadius = "10px";
+    document.getElementById("searchbar").style.borderTopRightRadius = "10px";
+    document.getElementById("sugen").style.display = "none";
+    document.getElementById("searchbar").style.height = "55px";
+    document.getElementById("info").style.display = "flex";
+    removeandcorrect()
+}
+function removeandcorrect() {
+    document.getElementById("google").setAttribute("onclick", "googlesearchforlogic(),googlesearch()")
+    document.getElementById("info").setAttribute("onclick", "informationsearchforlogic()")
+    document.getElementById("bing").setAttribute("onclick", "bingsearchforlogic(),bingsearch()")
+    document.getElementById("brave").setAttribute("onclick", "bravesearchforlogic(),bravesearch()")
+    document.getElementById("yahoo").setAttribute("onclick", "yahoosearchforlogic(),yahoosearch()")
+    document.getElementById("duckduckgo").setAttribute("onclick", "duckduckgosearchforlogic(),duckduckgosearch()")
+    document.getElementById("metager").setAttribute("onclick", "metagersearchforlogic(),metagersearch()")
 }
--- js/search.js
+++ js/search.js
@@ -31,7 +31,7 @@ function search() {
         window.open("https://in.search.yahoo.com/search;_ylt=Awr1Tfk6LyNkpswTTja6HAx.;_ylc=X1MDMjExNDcyMzAwMgRfcgMyBGZyAwRmcjIDc2ItdG9wLXNlYXJjaARncHJpZAMxSEM0dUtjbVJ2cWouRGJNYzlnbjBBBG5fcnNsdAMwBG5fc3VnZwM5BG9yaWdpbgNpbi5zZWFyY2gueWFob28uY29tBHBvcwMwBHBxc3RyAwRwcXN0cmwDMARxc3RybAM2BHF1ZXJ5A2dvb2dsZQR0X3N0bXADMTY4MDAyNzQ1Mw--?p=" + input, "_self")
     }
     else if (engine === "DuckDuckGo") {
-        window.open("https://duckduckgo.com/q=" + input, "_self")
+        window.open("https://duckduckgo.com/" + input, "_self")
     }
     else if (engine === "Metager") {
         window.open("https://metager.org/meta/meta.ger3?eingabe=" + input, "_self")
