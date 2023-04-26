ef2022231acc8351a45220e27f8840e681571145
Souvik Nandi <83856385+SouvikNandi2004@users.noreply.github.com>
2023-04-27 00:41:11 +0530

    Add files via upload

--- components/search/src/js/logic1.js
+++ components/search/src/js/logic1.js
@@ -1,14 +1,15 @@
 function mainload() {
+
     document.getElementById("___gcse_0").setAttribute("id", "main_disunic")
     // Tab Bar
     let tabbar = document.getElementsByClassName("gsc-tabsArea")
-    try{
+    try {
         tabbar[0].innerHTML = `<div class="tabs">
-                                    <div class="tb" id="sweb" onclick='sweb()' ">Web</div>
+                                    <div class="tb" id="sweb" onclick='sweb()'">Web</div>
                                     <div class="tb" id="simg" onclick='simg()'>Images</div>
                                 </div>`;
     }
-    catch{
+    catch {
         // window.open("https://disunic.github.io/","_self")
         console.log("error")
     }
@@ -16,7 +17,10 @@ function mainload() {
     const buttonimg = document.getElementsByClassName("gsc-search-button-v2");
     buttonimg[0].innerHTML = "<i class='bi bi-search'></i>";
     let query = document.getElementById("gsc-i-id1").value;
+    wiki()
+    document.getElementsByClassName("gsc-search-button-v2")[0].setAttribute("onclick", "wiki()");
 }
+
 // Tab Bar
 function sweb() {
     localStorage.setItem("tab", "web")
@@ -29,4 +33,33 @@ function simg() {
     checkonload2()
     let query = document.getElementById("gsc-i-id1").value;
     window.open("https://disunic.github.io/search.html#gsc.tab=1&gsc.sort=&gsc.q=" + query, "_self")
+}
+
+function wiki() {
+    setTimeout(() => {
+        var userInput = $("#gsc-i-id1").val();
+        // var userInput = document.getElementById("gsc-i-id1").value
+        $.getJSON("https://en.wikipedia.org/w/api.php?format=json&action=query&generator=search&redirects=1&gsrnamespace=0&gsrlimit=1&prop=pageimages|extracts&pilimit=max&exintro&pithumbsize=400&explaintext&exsentences=8&exlimit=max&gsrsearch=" + encodeURIComponent(userInput) + "&callback=?", function (data) {
+            try {
+                wikiaddsenginedisunic()
+                var pages = data.query.pages;
+                var resultHtml = "";
+                $.each(pages, function (id, page) {
+                    resultHtml += "<img src='" + page.thumbnail.source + "' alt='" + page.title + "'>";
+                    resultHtml += "<p>" + page.extract + "</p>";
+                    resultHtml += "<a href='https://en.wikipedia.org/?curid=" + page.pageid + "' target='_blank'>Read more</a>";
+                });
+                console.clear()
+                logo()
+                $("#wikidisunic").html(resultHtml);
+            }
+            catch {
+                document.getElementById("wikidisunic").remove()
+            }
+        });
+    }, 500);
+
+}
+function wikiaddsenginedisunic() {
+    $(".gsc-webResult").eq(1).after("<div class='gsc-webResult gsc-result' id='wikidisunic'></div>");
 }
